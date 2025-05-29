a9f51f8f8c9c46db28f576b2a19fe134070593d2
livia <liviagalvao2009@gmail.com>
2025-05-29 16:37:56 -0300

    1

---
+++ 1.js
@@ -0,0 +1,12 @@
+//1. Digite seu nome de usuário
+
+let usuario, senha;
+usuario = prompt ("Usuário:")
+senha = prompt ("Senha:")
+while( usuario !== "livia" || senha !== "sccp" ){
+    console.log ("")
+    usuario = prompt ("")
+    senha = prompt ("")
+    
+} 
+console.log ("Bem-vindo!")

284c488c873a861f64a7451d5ddf24987d8a27c3
livia <liviagalvao2009@gmail.com>
2025-05-29 16:39:17 -0300

    2

---
+++ 2.js
@@ -0,0 +1,11 @@
+
+//2. Criação de senha válida
+
+let senha ;
+senha = prompt ("Senha: ")
+while (senha !== "sccp"){
+        console.log ("Digite novamente.")
+     senha = prompt ("Senha: ")
+    
+} 
+console.log ("Bem-vindo!")
